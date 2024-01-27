cd6fe365e10346bf4c32f49c87b8b39fda6d6b0a
B0realian <142990172+B0realian@users.noreply.github.com>
2024-01-27 22:35:34 +0100

    Updated menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,22 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject quit;
+    public GameObject credits;
+    public GameObject how;
+    public GameObject play;
+
+    AudioSource audiosource;
+    public AudioClip music;
+    
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        audiosource.clip = music;
+        audiosource.Play();
+        quit.SetActive(false);
+        credits.SetActive(false);
+        how.SetActive(false);
+        play.SetActive(false);
+    }
+
     public void PlayGame()
+    {
+        menu.SetActive(false);
+        play.SetActive(true);
+        Invoke("StartGame", 3);
+    }
+
+    private void StartGame()
     {
         SceneManager.LoadSceneAsync("Philip");
     }
 
-  
-    public void AboutGame()
+    public void HowTo()
     {
-        SceneManager.LoadSceneAsync("TeamAwesome");
+        menu.SetActive(false);
+        how.SetActive(true);
+        Invoke("Instructions", 3);
+    }
+
+    private void Instructions()
+    {
+        // Show instructions
+    }
+
+    public void Credits()
+    {
+        menu.SetActive(false);
+        credits.SetActive(true);
+        Invoke("AboutGame", 3);
+    }
+
+    private void AboutGame()
+    {
+        // Show credits
     }
 
     public void ExitGame()
+    {
+        menu.SetActive(false);
+        quit.SetActive(true);
+        Invoke("Quitting", 3);
+    }
+
+    private void Quitting()
     {
         Application.Quit();
     }
